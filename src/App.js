0f3fa64b48a661b243d3787c553423b5c9c24ebd
Cid Medeiros <46410491+cidmedeiros@users.noreply.github.com>
2021-03-11 19:21:57 -0300

    add call functions to the contract

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
 import lottery from './lotteryContract';
+import web3 from './web3';
 
 /* Time Line on handling Contracts:
       * Component renders ->
@@ -12,13 +13,20 @@ class App extends Component {
   constructor(props){
     super(props);
 
-    this.state = {manager:''};
+    this.state = {
+      manager:'',
+      players: [],
+      balance: ''
+    };
   }
   //componentDidMount() is a lifecycle method, a hook that gets invoked right after a React component has been mounted aka after the first render() lifecycle.
   async componentDidMount(){
     const manager = await lottery.methods.manager().call();
+    const players = await lottery.methods.getPlayers().call();
+    const acc = await web3.eth.getAccounts();
+    const balance = await web3.eth.getBalance(acc[0]);
 
-    this.setState({manager})//ES6 -> {manager:manager}
+    this.setState({manager, players, balance});//ES6 -> {manager:manager, players:players}
   }
 
   //the Reactor render method accpets promises right off the batch
@@ -26,7 +34,20 @@ class App extends Component {
     return (
       <div>
         <h2>Lottery Contract</h2>
-        <p>This Contract is managed by {this.state.manager} </p>
+        <p>
+        This Contract is managed by {this.state.manager}. There are currently  {this.state.players.length}  people entered the lottery,
+        competing to win {web3.utils.fromWei(this.state.balance, 'ether')} ether!
+        </p>
+        <hr />
+        <form>
+          <h4>Want to try your lucky?</h4>
+          <div>
+            <label></label>
+            <input
+              onChange={event => this.setState}
+            />
+          </div>
+        </form>
       </div>
     );
   }
